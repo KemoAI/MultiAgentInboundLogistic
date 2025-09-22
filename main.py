2ef117b9432ed1df80fa651fa1dd9bb9b1388908
kamalmohammed <kamal.mohammed@innovate-mea.com>
2025-09-22 16:39:50 +0300

    22 Commit

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from multiagentinboundlogistic!")
+# filename: main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+from src.utils import format_message
+from langchain_core.messages import HumanMessage
+from src.full_agent import full_agent
 
+# Define the request body schemaad
+class UserRequest(BaseModel):
+      usermessage: str
+      username: str
 
-if __name__ == "__main__":
-    main()
+# Initialize FastAPI app
+app = FastAPI()
+
+# Define the POST endpoint
+@app.post("/submit-message")
+async def submit_message(request: UserRequest):
+    thread = {"configurable":{"thread_id":request.username}}
+    result = await full_agent.ainvoke({"messages":[HumanMessage(content=request.usermessage)]} , config=thread)
+    return {"message": result["messages"]}
